d269264e806d5952e50c985d6d114b048150d534
yeo-chi <00www.black@gmail.com>
2024-01-09 16:37:31 +0900

    init

---
+++ src/main/kotlin/yeo/chi/reactive_user/ReactiveUserApplication.kt
@@ -0,0 +1,11 @@
+package yeo.chi.reactive_user
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReactiveUserApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReactiveUserApplication>(*args)
+}
---
+++ src/test/kotlin/yeo/chi/reactive_user/ReactiveUserApplicationTests.kt
@@ -0,0 +1,13 @@
+package yeo.chi.reactive_user
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReactiveUserApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
